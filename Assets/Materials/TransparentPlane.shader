6e5d8a35c16c58dcddf4d062f3024bf7c5ea4c27
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-03-09 08:47:07 -0500

    Decal Texture

---
+++ Assets/Materials/TransparentPlane.shader
@@ -0,0 +1,18 @@
+﻿Shader "Custom/TransparentPlane"
+{
+    Properties
+	{
+        _MainTex ("Texture", 2D) = "black" {}
+       
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha
+        Pass
+			 {
+		      SetTexture[_MainTex] {combine texture}
+			 }
+    }
+    
+}
---
+++ Assets/Scripts/ShowDecal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class ShowDecal : MonoBehaviour
+{
+    bool showDecal=false;
+    Material decalMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        decalMaterial = GetComponent<Renderer>().sharedMaterial;
+    }
+    private void OnMouseDown()
+    {
+        showDecal = !showDecal;
+        if (showDecal)
+        {
+            decalMaterial.SetFloat("_ShowDecal", 1);
+        }
+        else
+        {
+            decalMaterial.SetFloat("_ShowDecal", 0);
+        }
+    }
+   
+}
