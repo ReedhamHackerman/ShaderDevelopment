7e12760766c6da40ca1e6761f7291b26b6fb378e
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-03-09 20:34:49 -0500

    Extrude , Glass And Water Effects
    Extrude ,Glass and Water effect added to the Game

---
+++ Assets/BombEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class BombEffect : MonoBehaviour
+{
+    Material mat;
+    public float rateOfChange = 0.1f;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    private void OnMouseDown()
+    {
+        float a = mat.GetFloat("_Amount");
+        if (a>1.0)
+        {
+            return;
+        }
+        else
+        {
+            a += rateOfChange;
+            mat.SetFloat("_Amount", a);
+        }
+    }
+    private void DecomposeMesh()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            float a = mat.GetFloat("_Amount");
+            if (a < -1.0)
+            {
+                return;
+            }
+            else
+            {
+                a -= rateOfChange;
+                mat.SetFloat("_Amount", a);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        DecomposeMesh();
+    }
+
+}
